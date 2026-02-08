077122dbe6b774bc332483060c5fb1b7afed4d6f
BorysOlmos <sanadoheridasf@gmail.com>
2026-02-08 17:03:09 -0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,20 @@
-const CACHE_NAME = 'panpos-v1';
-const ASSETS = ['./', './index.html', './app.js', './estilos.css', './manifest.json', 'https://unpkg.com/dexie/dist/dexie.js'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE_NAME = 'panpos-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './app.js',
+  './estilos.css',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  // SI LA PETICIÓN ES PARA GOOGLE, NO USAR CACHE
+  if (e.request.url.includes('script.google.com')) {
+    return fetch(e.request);
+  }
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
